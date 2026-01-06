d72eb70eb1dc1e5753f05e5de21817d7aa2932f2
@vishwajeetsharma09 <vishusharmaiit09@gmail.com>
2026-01-06 16:33:39 +0530

    initial

--- allpattern.cpp
+++ allpattern.cpp
@@ -1,5 +1,14 @@
 #include<iostream>
 using namespace std;
 int main(){
+    int n= 5;
+    char ch = 'A';
+    for(int i=0; i<n; i++){
+        for( int j= 0; j<= i;j++){
+            cout<<ch<<" ";
+        }
+        ch++;
+        cout<<endl;
+    }
  
 }
